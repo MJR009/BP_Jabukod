c685ec652a234ebbb3dcef1724e85f9a5e05df42
MJR009 <marty.jab@seznam.cz>
2025-04-23 14:48:25 +0200

    Documented common.h

--- project/common.h
+++ project/common.h
@@ -1,3 +1,12 @@
+/**
+ * @file common.h
+ * @author Martin Jabůrek
+ *
+ * @brief Header to be available in every source file.
+ * 
+ * Contains all top level definitions, includes and macros, desired to be available everywhere in code.
+ */
+
 #pragma once
 
 #include <iostream>
@@ -20,29 +29,55 @@
 
 using namespace std;
 
+/**
+ * @name Error codes. Sometimes used as placeholders for return type "any" not being used.
+ * @{
+ */
 #define OK 0
 #define NOK 1
+/** @} */
 
+/**
+ * @name Text characteristics ANSI escape sequences.
+ * @{
+ */
 #define BOLD "\033[1m"
 #define DIM "\033[2m"
 #define EMPH "\033[3m"
+/** @} */
 
+/**
+ * @name Text color ANSI escape sequences.
+ * @{
+ */
 #define RED "\033[31m"
 #define YELLOW "\033[33m"
 #define ORANGE "\033[38;2;255;165;0m"
 #define CYAN "\033[36m"
 #define MAGENTA "\033[35m"
 #define TEAL "\033[38;2;77;214;164m"
+/** @} */
 
+/// @name Resets text to default after ANSI escape sequence usage.
 #define DEFAULT "\033[0m"
 
+/**
+ * @name Table border characters. Used for printing abstract syntax trees.
+ * @{
+ */
 #define VERTICAL "\u2502"
 #define HORIZONTAL "\u2500"
 #define FORK "\u251c"
 #define STOP "\u2514"
+/** @} */
 
 namespace ERR {
 
+/**
+ * @brief Function to be called when an error occurs in structure of generated internal code representation.
+ * 
+ * When called, prints a highlighted message.
+ */
 void BadData();
 
 } // namespace ERR
